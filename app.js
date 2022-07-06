c166a684e139b7f4c1763c74b787c00654bb4a11
Your Name <you@example.com>
2022-07-05 20:06:47 -0600

    added java

---
+++ app.js
@@ -0,0 +1,11 @@
+let username = "tyler";
+console.log(username);
+
+let favcolour = "red";
+console.log(favcolour);
+
+let userdob = 1997;
+console.log(userdob);
+
+let userage = 26;
+console.log(userage);
